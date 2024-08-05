c7a008ce7e08c58216d18b54ae6e6ec1e09cafbf
Mike <msutherland@fastmail.com>
2024-08-05 00:31:11 -0700

    update pressure sensor example to show altitude

--- ollyfc/examples/bmp388.rs
+++ ollyfc/examples/bmp388.rs
@@ -1,14 +1,13 @@
 #![no_main]
 #![no_std]
-#![feature(type_alias_impl_trait)]
-#![feature(generic_arg_infer)]
-#![feature(stmt_expr_attributes)]
 
 use defmt_rtt as _;
 use panic_probe as _;
 
 use bmp388::{Blocking, BMP388};
+use num_traits::Pow;
 use ollyfc::sensor::pressure::BMP388_CFG;
+use rtic_monotonics::{systick::Systick, Monotonic};
 use stm32f4xx_hal::prelude::*;
 use stm32f4xx_hal::{i2c, pac};
 
@@ -16,7 +15,6 @@ use stm32f4xx_hal::{i2c, pac};
 mod app {
 
     use super::*;
-    use rtic_monotonics::{systick::Systick, Monotonic};
 
     #[shared]
     struct Shared {}
@@ -28,9 +26,9 @@ mod app {
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
-        let mut dp = cx.device;
+        let dp = cx.device;
         let rcc = dp.RCC.constrain();
-        let hse = 25.MHz();
+        let hse = 12.MHz();
         let sysclk = 64.MHz();
         let clocks = rcc
             .cfgr
@@ -39,18 +37,20 @@ mod app {
             .require_pll48clk()
             .freeze();
 
-        let mut syscfg = dp.SYSCFG.constrain();
+        let _syscfg = dp.SYSCFG.constrain();
         let systick_mono_token = rtic_monotonics::create_systick_token!();
-        Systick::start(cx.core.SYST, 64_000_000, systick_mono_token);
+        Systick::start(cx.core.SYST, sysclk.to_Hz(), systick_mono_token);
 
         let gpioa = dp.GPIOA.split();
         let gpioc = dp.GPIOC.split();
 
-        let i2c3_sda = gpioc.pc9.into_alternate_open_drain();
-        let i2c3_scl = gpioa.pa8.into_alternate_open_drain();
-        let i2c3 = dp.I2C3.i2c((i2c3_scl, i2c3_sda), 400.kHz(), &clocks);
+        let scl = gpioa.pa8;
+        let sda = gpioc.pc9;
+        let i2c3 = i2c::I2c::new(dp.I2C3, (scl, sda), i2c::Mode::standard(400.kHz()), &clocks);
+
         let mut delay = dp.TIM2.delay_ms(&clocks);
         let cfg = BMP388_CFG;
+        defmt::info!("cfg address={:x}", cfg.address);
         let sensor = cfg.setup_blocking(i2c3, &mut delay).unwrap();
 
         read_sensor::spawn().unwrap();
@@ -63,12 +63,20 @@ mod app {
         let sensor = cx.local.sensor;
         loop {
             let now = rtic_monotonics::systick::Systick::now();
-
             let data = sensor.sensor_values().unwrap();
-            defmt::info!("Pressure: {} Pa", data.pressure);
-            defmt::info!("Temperature: {} C", data.temperature);
+            defmt::info!(
+                "Altitude: {} m {} Pa {} C",
+                altitude(data.pressure),
+                data.pressure,
+                data.temperature
+            );
 
             rtic_monotonics::systick::Systick::delay_until(now + 20u32.millis()).await;
         }
     }
+}
+
+pub fn altitude(pressure: f64) -> f64 {
+    let altitude = 44307.69396 * (1.0 - (pressure / 101325.0).pow(0.190284));
+    altitude
 }
